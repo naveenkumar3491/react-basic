96800ad069c0ff20556c427542ba49d3316c4856
Pamala Naveen Kumar <E003672@cigniti.com>
2017-07-05 19:31:40 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 //react packages are installed through package.json
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Show = require('./show');
+var shows = require('./shows');
+
+
+
+ReactDOM.render(
+    <Show showIndex={1} shows={shows} />,
+    document.getElementById('root')
+);
 
 //first param - element to create, second param- attributes any on the element needed, 
 //third param - content children which needs to sit inside the element which is abt to create
@@ -8,23 +17,47 @@ var ReactDOM = require('react-dom');
 
 //var App = <h1>Hello World</h1>;
 
-var Component = function(props){
-    return (
-        <div style={{color: props.color}}>  
-            <h1>{props.greeting}</h1>
-        </div>
-    );
-};
+// var Component = function(props){
+//     return (
+//         <div style={{color: props.color}}>  
+//             <h1>{props.greeting}</h1>
+//         </div>
+//     );
+// };
+
+// var Component = createClass({
+//     getInitialState: function () {
+//         return {
+//             color: 'blue'
+//         }
+//     },
+//     handleButtonClick: function(){
+//         this.setState(function(prevState){
+//             return {
+//                 color: (prevState.color === 'blue') ? 'green' : 'blue'
+//             }
+//         });
+//     },
+//     render: function () {
+//         console.log('inside render'+ this.state.color)
+//         return (
+//             <div>
+//                 <div style={{ color: this.state.color }}>
+//                     <h1>{this.props.greeting}</h1>
+//                 </div>
+//                 <button onClick={this.handleButtonClick}>Click Me</button>
+//             </div>
+//         );
+//     }
+// });
 
 //root is the mount point which is mentioned in the index.html
 //ReactDOM.render(Component, document.getElementById('root'));
 
 //ReactDOM.render(React.createElement(Component), document.getElementById('root'));
 //ReactDOM.render(<Component></Component>, document.getElementById('root'));
-ReactDOM.render(
-    <div>
-    <Component greeting="Hello World" color="green"/>
-    <Component greeting="Hello there" color="blue"/>
-    </div>,
-     document.getElementById('root')
-);
+
+// ReactDOM.render(
+//     <Component greeting="Hello World" />,
+//     document.getElementById('root')
+// );
---
+++ src/show.js
@@ -0,0 +1,64 @@
+var React = require('react');
+var createClass = require('create-react-class');
+
+var Title = createClass({
+    render: function () {
+        return (
+            <h3>Title: {this.props.showTitle}</h3>
+        );
+    }
+});
+
+
+var Poster = createClass({
+    render: function () {
+        return (
+            <img src={this.props.showPoster} alt="Show Poster" style={{ height: 400, width: 400 }} />
+        )
+    }
+});
+
+var ShowInfo = createClass({
+    render: function () {
+        return (
+            <div>
+                <p>Plot: {this.props.showPlot}</p>
+                <h5>IMDB Rating: {this.props.showRating}</h5>
+            </div>
+        );
+    }
+});
+
+var Show = createClass({
+    getDefaultProps: function(){
+        return{
+            showIndex: 0
+        }
+    },
+    getInitialState: function(){
+        return{
+            showIndex: this.props.showIndex 
+        }
+    },
+    handleShowButton: function(){
+        var totalShows = this.props.shows.length;
+        this.setState(function(prevState){
+            return{
+                 showIndex: (prevState.showIndex + 1) % totalShows
+            }
+        });
+    },
+    render: function () {
+        var show = this.props.shows[this.state.showIndex];
+        return (
+            <div className="text-center">
+                <Title showTitle={show.title}/>
+                <Poster showPoster={show.poster}/>
+                <ShowInfo showPlot={show.plot} showRating={show.imdbRating}/>
+                <button onClick={this.handleShowButton}>Next Show</button>
+            </div>
+        );
+    }
+});
+
+module.exports = Show;
